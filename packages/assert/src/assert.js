03038691dafd9d3d8213c0903416f8ae2501ec83
Mark S. Miller <erights@gmail.com>
2020-10-19 14:52:39 -0700

    better but still DOES NOT WORK

--- packages/assert/src/assert.js
+++ packages/assert/src/assert.js
@@ -1,10 +1,11 @@
+/* global globalThis */
 // Copyright (C) 2019 Agoric, under Apache License 2.0
 
 // @ts-check
 
+import 'ses';
 import './types';
 
-import { assert } from 'ses/src/error/assert';
-
+const assert = globalThis.assert;
 const { details, quote } = assert;
 export { assert, details, quote, quote as q };
