d59359a7bb49cf11420783b2e13ff658babb4394
Kate Sills <kate@agoric.com>
2020-10-23 17:10:43 -0700

    chore: clean up

--- packages/zoe/exported.js
+++ packages/zoe/exported.js
@@ -1,6 +1,7 @@
 import './src/contractFacet/types';
 import './src/zoeService/types';
 import './src/contractSupport/types';
+import './src/contracts/exported';
 import './src/types';
 import '@agoric/ertp/exported';
 import '@agoric/store/exported';
